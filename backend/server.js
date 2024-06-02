20d0a98725a7707a22371def510830eae7b5a925
Sandip-Chavda <chavdas766@gmail.com>
2024-06-02 17:11:01 +0530

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import path from "path";
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
@@ -7,6 +8,8 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
